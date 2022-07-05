a1280c5f0089a725ba7ab8be4003049084466cbc
MHAE391 <modatherelgohary391@gmail.com>
2022-07-05 23:28:59 +0200

    update

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import cv2 as cv
 from matplotlib import pyplot as plt
-img = cv.imread('Large.png')
+img = cv.imread('7.jpeg')
 img2 = img.copy()
-template = cv.imread('Small.png')
+template = cv.imread('fullLogo.jpeg')
 w, h = template.shape[:2]
 # All the 6 methods for comparison in a list
 methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
@@ -16,9 +16,9 @@ for meth in methods:
         top_left = min_loc
     else:
         top_left = max_loc
-    bottom_right = (top_left[0] + w, top_left[1] + h)
+    bottom_right = (top_left[0] + h, top_left[1] + w)
     cv.rectangle(img,top_left, bottom_right, 255, 2)
-    cropped_image = img[bottom_right[0]:bottom_right[1],top_left[0]:top_left[1]]
+    cropped_image = img[top_left[1]:bottom_right[1],top_left[0]:bottom_right[0]]
     plt.subplot(122), plt.imshow(cropped_image)
     plt.title('Cropped'), plt.xticks([]), plt.yticks([])
     plt.subplot(121), plt.imshow(img)

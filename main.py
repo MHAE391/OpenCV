0638b6462c812cf8f828f69889c47f868b009468
MHAE391 <modatherelgohary391@gmail.com>
2022-07-05 23:10:27 +0200

    Update

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
-import cv2
-
-
-small = cv2.imread('Small.png')   #بيقرا الصوره الصغيره اللي  هي اللجو اللي  انا هدور عليه ف الصوره الكبيره
-large = cv2.imread('Large.png')   # بيقرا الصوره الكبيره اللي هيدور فيها ع اللوجو
-(height1, width1) = small.shape[:2]  #بحسب الطول و العرض بتوع الصوره اللوجو
-(height2, width2) = large.shape[:2]  #بحسب الطول و العرض بتوع الصوره الكبيره
-small_image = cv2.resize(small, (int(width1 / 2), int(height1 / 2)), interpolation = cv2.INTER_CUBIC) #بصغر حجم الصوره للنص عشان كانت  كبيره
-large_image = cv2.resize(large, (int(width2 / 2), int(height2 / 2)), interpolation = cv2.INTER_CUBIC) #بصغر حجم الصوره للنص عشان كانت  كبيره
-
-
-method = cv2.TM_SQDIFF_NORMED    # الطريقه اللي هنقارن بيها بين الصورتين
-
-res = cv2.matchTemplate(small,large,method)  # بيجيب مكان اللوجو ف الصوره الكبيره
-mn1,_,mnloc1,_=cv2.minMaxLoc(res)  # بيجيب مكان البدايه والنهايه
-mpx,mpy=mnloc1 # بيجيب مكان البدايه والنهايه
-trows1,tcols1 = small.shape[:2] # بيجيب الكول  والعرض بتوع الصوره الصغيره
-
-#الاربع سطور اللي جايين دول  نفس اللي فوق  بس بينفذهم ع الصوره بعد ما غيرت حجمها
-result = cv2.matchTemplate(small_image, large_image, method)
-mn,_,mnLoc,_ = cv2.minMaxLoc(result)
-MPx,MPy = mnLoc
-trows,tcols = small_image.shape[:2]
-
-cropped_image = large [mpy:mpy+trows1,mpx:mpx+tcols1] # بيقص اللوجو من الصوره الكبيره ويعرضه
-cv2.imshow("Res", cropped_image)
-
-
-cv2.rectangle(large_image, (MPx,MPy),(MPx+tcols,MPy+trows),(0,0,255),2) # بيرسم المستطيل حولين اللوجو في الصوره الكبيره
-cv2.imshow('Result',large_image)
-cv2.waitKey(0)
+import cv2 as cv
+from matplotlib import pyplot as plt
+img = cv.imread('Large.png')
+img2 = img.copy()
+template = cv.imread('Small.png')
+w, h = template.shape[:2]
+# All the 6 methods for comparison in a list
+methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
+            'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
+for meth in methods:
+    img = img2.copy()
+    method = eval(meth)
+    res = cv.matchTemplate(img,template,method)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+    if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
+        top_left = min_loc
+    else:
+        top_left = max_loc
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+    cv.rectangle(img,top_left, bottom_right, 255, 2)
+    cropped_image = img[bottom_right[0]:bottom_right[1],top_left[0]:top_left[1]]
+    plt.subplot(122), plt.imshow(cropped_image)
+    plt.title('Cropped'), plt.xticks([]), plt.yticks([])
+    plt.subplot(121), plt.imshow(img)
+    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+    plt.suptitle(meth)
+    plt.show()

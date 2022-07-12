26202215473e50961104f5aebfa1d1f377754e61
MHAE391 <modatherelgohary391@gmail.com>
2022-07-12 21:22:42 +0200

    add

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import cv2
 cap = cv2.VideoCapture('vv.mp4')
-template = cv2.imread('fullLogo.jpeg')
+template = cv2.imread('logoo.jpeg')
 w, h = template.shape[:2]
+
 if (cap.isOpened()== False):
   print("Error opening video stream or file")
 
@@ -10,10 +11,12 @@ while(cap.isOpened()):
   if ret == True:
 
     frame=cv2.rotate(frame, cv2.ROTATE_180)
-    res = cv2.matchTemplate(frame, template, cv2.TM_CCORR_NORMED)
+    res = cv2.matchTemplate(frame, template, cv2.TM_CCOEFF)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
     top_left = max_loc
-    bottom_right = (top_left[0] + h, top_left[1] + w)
+    bottom_right = (top_left[0] + 270, top_left[1] + 250)
+    print(top_left)
+    print(bottom_right)
     cv2.rectangle(frame, top_left, bottom_right, (0, 255, 0), 2)
     cv2.imshow('Frame',frame)
 

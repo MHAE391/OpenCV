d33edadc53d16776f29c796b4daf09135d7cf68c
MHAE391 <modatherelgohary391@gmail.com>
2022-07-12 17:11:46 +0200

    Try

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
+import numpy as np
 import cv2 as cv
-from matplotlib import pyplot as plt
-img = cv.imread('7.jpeg')
-img2 = img.copy()
+
+cap=cv.VideoCapture(0)
 template = cv.imread('fullLogo.jpeg')
 w, h = template.shape[:2]
-# All the 6 methods for comparison in a list
-methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
-            'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
-for meth in methods:
-    img = img2.copy()
-    method = eval(meth)
-    res = cv.matchTemplate(img,template,method)
+while True:
+    ret,frame=cap.read()
+    res = cv.matchTemplate(frame, template, cv.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-    if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
-        top_left = min_loc
-    else:
-        top_left = max_loc
+    top_left = max_loc
     bottom_right = (top_left[0] + h, top_left[1] + w)
-    cropped_image = img[top_left[1]:bottom_right[1],top_left[0]:bottom_right[0]]
-    plt.axis("off")
-    plt.subplot(122)
-    plt.title('Cropped'), plt.xticks([]), plt.yticks([])
-    plt.imshow(cv.cvtColor(cropped_image, cv.COLOR_BGR2RGB))
-    plt.subplot(121)
-    cv.rectangle(img, top_left, bottom_right, (0, 0, 255), 8)
-    plt.imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
-    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-    plt.suptitle(meth)
-    plt.show()
+    cv.rectangle(frame, top_left, bottom_right, (0, 0, 255), 4)
+    cv.imshow('frame',frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
